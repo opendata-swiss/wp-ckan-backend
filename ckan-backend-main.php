ebfd528e96f9b573bc622b2e16d024908e08b8f3
Juerg Hunziker <juerg.hunziker@liip.ch>
2015-06-26 11:04:19 +0200

    define dependencies in array

--- ckan-backend-main.php
+++ ckan-backend-main.php
@@ -8,7 +8,6 @@ class Ckan_Backend_Main {
 	protected $version;
 
 	public function __construct() {
-
 		$this->plugin_slug = 'ckan-backend';
 		$this->version = '1.0.0';
 
@@ -19,15 +18,21 @@ class Ckan_Backend_Main {
 		$local_organisation = new Ckan_Backend_Local_Organisation();
 	}
 
-	private function load_dependencies() {
-		require_once plugin_dir_path( __FILE__ ) . 'helper/ckan-backend-helper.php';
-		require_once plugin_dir_path( __FILE__ ) . 'post-types/ckan-backend-local-dataset.php';
-		require_once plugin_dir_path( __FILE__ ) . 'post-types/ckan-backend-local-group.php';
-		require_once plugin_dir_path( __FILE__ ) . 'post-types/ckan-backend-local-organisation.php';
-		require_once plugin_dir_path( __FILE__ ) . 'sync/ckan-backend-sync-abstract.php';
-		require_once plugin_dir_path( __FILE__ ) . 'sync/ckan-backend-sync-local-dataset.php';
-		require_once plugin_dir_path( __FILE__ ) . 'sync/ckan-backend-sync-local-group.php';
-		require_once plugin_dir_path( __FILE__ ) . 'sync/ckan-backend-sync-local-organisation.php';
+	protected function load_dependencies() {
+		$depedency_file_paths = array(
+			'helper/ckan-backend-helper.php',
+			'post-types/ckan-backend-local-dataset.php',
+			'post-types/ckan-backend-local-group.php',
+			'post-types/ckan-backend-local-organisation.php',
+			'sync/ckan-backend-sync-abstract.php',
+			'sync/ckan-backend-sync-local-dataset.php',
+			'sync/ckan-backend-sync-local-group.php',
+			'sync/ckan-backend-sync-local-organisation.php'
+		);
+
+		foreach($depedency_file_paths as $file_path) {
+			require_once plugin_dir_path( __FILE__ ) . $file_path;
+		}
 	}
 
 	public function get_version() {
