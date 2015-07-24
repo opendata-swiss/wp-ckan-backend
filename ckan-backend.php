7409f91c6cff90b37763b5d4328e2ac851ae89d9
Juerg Hunziker <juerg.hunziker@liip.ch>
2015-07-24 09:30:37 +0200

    handle file upload

--- ckan-backend.php
+++ ckan-backend.php
@@ -45,9 +45,10 @@ if ( ! class_exists( 'Ckan_Backend', false ) ) {
 		public function bootstrap() {
 			$this->load_dependencies();
 
-			$local_dataset      = new Ckan_Backend_Local_Dataset();
-			$local_group        = new Ckan_Backend_Local_Group();
-			$local_organisation = new Ckan_Backend_Local_Organisation();
+			new Ckan_Backend_Local_Dataset();
+			new Ckan_Backend_Local_Group();
+			new Ckan_Backend_Local_Organisation();
+			new Ckan_Backend_Local_Dataset_Import();
 		}
 
 		public function add_scripts() {
--- post-types/ckan-backend-local-dataset-import.php
+++ post-types/ckan-backend-local-dataset-import.php
@@ -1,56 +1,94 @@
 <?php
 
-add_action('admin_menu', 'register_ckan_local_dataset_import_submenu_page');
+class Ckan_Backend_Local_Dataset_Import {
 
-function register_ckan_local_dataset_import_submenu_page() {
-	add_submenu_page(
-		'edit.php?post_type=' . Ckan_Backend_Local_Dataset::POST_TYPE,
-		__( 'Import CKAN Dataset', 'ogdch' ),
-		__( 'Import', 'ogdch' ),
-		'manage_options',
-		'ckan-local-dataset-import-page',
-		'ckan_local_dataset_import_page_callback'
-	);
-}
+	public $menu_slug = 'ckan-local-dataset-import-page';
 
-function ckan_local_dataset_import_page_callback() {
-	// must check that the user has the required capability
-	if (!current_user_can('manage_options'))
-	{
-		wp_die( __('You do not have sufficient permissions to access this page.') );
+	public function __construct() {
+		add_action( 'admin_menu', array( $this, 'register_submenu_page' ) );
 	}
 
-	$import_submit_hidden_field_name = 'ckan_local_dataset_import_submit';
-	$file_path_field_name = 'ckan_local_dataset_import_file_path';
 
-	// Handle import
-	if( isset($_POST[ $import_submit_hidden_field_name ]) && $_POST[ $import_submit_hidden_field_name ] == 'Y' ) {
-		// Read their posted value
-		$file_path = $_POST[ $file_path_field_name ];
+	public function register_submenu_page() {
+		add_submenu_page(
+			'edit.php?post_type=' . Ckan_Backend_Local_Dataset::POST_TYPE,
+			__( 'Import CKAN Dataset', 'ogdch' ),
+			__( 'Import', 'ogdch' ),
+			'manage_options',
+			$this->menu_slug,
+			array( $this, 'import_page_callback' )
+		);
+	}
 
-		// TODO import file
+	public function import_page_callback() {
+		// must check that the user has the required capability
+		if ( ! current_user_can( 'manage_options' ) ) {
+			wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
+		}
 
-		?>
-		<div class="updated"><p><strong><?php _e('Import successful', 'ogdch' ); ?></strong></p></div>
+		$import_submit_hidden_field_name = 'ckan_local_dataset_import_submit';
+		$file_field_name                 = 'ckan_local_dataset_import_file';
+
+		// Handle import
+		if ( isset( $_POST[ $import_submit_hidden_field_name ] ) && $_POST[ $import_submit_hidden_field_name ] == 'Y' ) {
+			// TODO import file
+			if(isset($_FILES[$file_field_name])) {
+				$this->handle_file_import($_FILES[$file_field_name]);
+			}
+			?>
+			<div class="updated"><p><strong><?php _e( 'Import successful', 'ogdch' ); ?></strong></p></div>
+		<?php } ?>
+		<div class="wrap">
+			<h2><?php _e( 'Import CKAN Dataset', 'ogdch' ); ?></h2>
+
+			<form enctype="multipart/form-data" action="" method="POST">
+				<input type="hidden" name="<?php echo $import_submit_hidden_field_name; ?>" value="Y">
+
+				<p><?php _e( "File:", 'ogdch' ); ?>
+					<input type="file" name="<?php echo $file_field_name; ?>" value="" size="20">
+				</p>
+				<hr/>
+
+				<p class="submit">
+					<input type="submit" name="Submit" class="button-primary" value="<?php esc_attr_e( 'Import' ) ?>"/>
+				</p>
+			</form>
+		</div>
+
 		<?php
 	}
 
-	echo '<div class="wrap">';
-		echo '<h2>' . __( 'Import CKAN Dataset', 'ogdch' ) . '</h2>';
-		?>
+	public function handle_file_import($file) {
+		try {
+			// Undefined | Multiple Files | $_FILES Corruption Attack
+			// If this request falls under any of them, treat it invalid.
+			if (
+				! isset( $file['error'] ) ||
+				is_array( $file['error'] )
+			) {
+				throw new RuntimeException( 'Invalid parameters.' );
+			}
 
-		<form name="form1" method="post" action="">
-			<input type="hidden" name="<?php echo $import_submit_hidden_field_name; ?>" value="Y">
+			// Check $file['error'] value.
+			switch ( $file['error'] ) {
+				case UPLOAD_ERR_OK:
+					break;
+				case UPLOAD_ERR_NO_FILE:
+					throw new RuntimeException( 'No file sent.' );
+				case UPLOAD_ERR_INI_SIZE:
+				case UPLOAD_ERR_FORM_SIZE:
+					throw new RuntimeException( 'Exceeded filesize limit.' );
+				default:
+					throw new RuntimeException( 'Unknown errors.' );
+			}
 
-			<p><?php _e("File:", 'ogdch' ); ?>
-			<input type="file" name="<?php echo $file_path_field_name; ?>" value="" size="20">
-			</p><hr />
-
-			<p class="submit">
-			<input type="submit" name="Submit" class="button-primary" value="<?php esc_attr_e('Import') ?>" />
-			</p>
-		</form>
-	</div>
-
-	<?php
+			$xml = simplexml_load_file($file['tmp_name']);
+			if( ! $xml ) {
+				throw new RuntimeException( 'Uploaded file is not a vaild XML file' );
+			}
+			print_r($xml);
+		} catch ( RuntimeException $e ) {
+			echo $e->getMessage();
+		}
+	}
 }
