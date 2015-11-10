5fe3dd3bcf5901243559631e28bfcf498f879a9d
Juerg Hunziker <juerg.hunziker@liip.ch>
2015-11-10 18:14:51 +0100

    implement needed custom fields for harvester

--- post-types/ckan-backend-local-harvester.php
+++ post-types/ckan-backend-local-harvester.php
@@ -21,19 +21,8 @@ class Ckan_Backend_Local_Harvester {
 		$this->register_post_type();
 		add_action( 'cmb2_init', array( $this, 'define_fields' ) );
 
-		// render additional field after main cmb2 form is rendered
-		add_action( 'cmb2_after_post_form_' . self::POST_TYPE . '-box', array( $this, 'render_addition_fields' ) );
-
 		// initialize local group sync
 		new Ckan_Backend_Sync_Local_Harvester( self::POST_TYPE, self::FIELD_PREFIX );
-	}
-
-	/**
-	 * Renders additional fields which aren't saved in database.
-	 */
-	public function render_addition_fields() {
-		// Field shows that the metadata is not yet saved in database -> get values from $_POST array
-		echo '<input type="hidden" id="metadata_not_in_db" name="metadata_not_in_db" value="1" />';
 	}
 
 	/**
@@ -74,27 +63,26 @@ class Ckan_Backend_Local_Harvester {
 			'show_in_nav_menus'   => true,
 			'can_export'          => true,
 			'has_archive'         => false,
-			'exclude_from_search' => false,
-			'publicly_queryable'  => true,
-			'rewrite'             => array( 'slug' => 'group' ),
+			'exclude_from_search' => true,
+			'publicly_queryable'  => false,
 			'map_meta_cap'        => true,
-			'capability_type'     => array( 'group', 'groups' ),
+			'capability_type'     => array( 'harvester', 'harvesters' ),
 			'capabilities'        => array(
-				'edit_posts'             => 'edit_groups',
-				'edit_others_posts'      => 'edit_others_groups',
-				'publish_posts'          => 'publish_groups',
-				'read_private_posts'     => 'read_private_groups',
-				'delete_posts'           => 'delete_groups',
-				'delete_private_posts'   => 'delete_private_groups',
-				'delete_published_posts' => 'delete_published_groups',
-				'delete_others_posts'    => 'delete_others_groups',
-				'edit_private_posts'     => 'edit_private_groups',
-				'edit_published_posts'   => 'edit_published_groups',
+				'edit_posts'             => 'edit_harvesters',
+				'edit_others_posts'      => 'edit_others_harvesters',
+				'publish_posts'          => 'publish_harvesters',
+				'read_private_posts'     => 'read_private_harvesters',
+				'delete_posts'           => 'delete_harvesters',
+				'delete_private_posts'   => 'delete_private_harvesters',
+				'delete_published_posts' => 'delete_published_harvesters',
+				'delete_others_posts'    => 'delete_others_harvesters',
+				'edit_private_posts'     => 'edit_private_harvesters',
+				'edit_published_posts'   => 'edit_published_harvesters',
 				'create_posts'           => 'create_groups',
 				// Meta capabilites assigned by WordPress. Do not give to any role.
-				'edit_post'              => 'edit_group',
-				'read_post'              => 'read_group',
-				'delete_post'            => 'delete_group',
+				'edit_post'              => 'edit_harvester',
+				'read_post'              => 'read_harvester',
+				'delete_post'            => 'delete_harvester',
 			),
 		);
 		register_post_type( self::POST_TYPE, $args );
@@ -124,32 +112,52 @@ class Ckan_Backend_Local_Harvester {
 			'id'   => 'harvester_information_title',
 		) );
 
-		foreach ( $language_priority as $lang ) {
-			$cmb->add_field( array(
-				'name'       => __( 'Title', 'ogdch' ) . ' (' . strtoupper( $lang ) . ')',
-				'id'         => self::FIELD_PREFIX . 'title_' . $lang,
-				'type'       => 'text',
-			) );
-
-			$cmb->add_field( array(
-				'name'       => __( 'Description', 'ogdch' ) . ' (' . strtoupper( $lang ) . ')',
-				'id'         => self::FIELD_PREFIX . 'description_' . $lang,
-				'type'       => 'textarea',
-				'attributes' => array( 'rows' => 3 ),
-			) );
-		}
-
-		/* Image */
 		$cmb->add_field( array(
-			'name' => __( 'Harvester Image', 'ogdch' ),
-			'type' => 'title',
-			'id'   => 'image_title',
+			'name' => __( 'URL', 'ogdch' ),
+			'id'   => self::FIELD_PREFIX . 'url',
+			'type' => 'text_url',
 		) );
 
 		$cmb->add_field( array(
-			'name'       => __( 'Image', 'ogdch' ),
-			'id'         => self::FIELD_PREFIX . 'image',
-			'type'    => 'file',
+			'name'       => __( 'Description', 'ogdch' ),
+			'id'         => self::FIELD_PREFIX . 'description',
+			'type'       => 'textarea_code',
+			'attributes' => array( 'rows' => 3 ),
+		) );
+
+		$cmb->add_field( array(
+			'name'    => __( 'Source type', 'ogdch' ),
+			'id'      => self::FIELD_PREFIX . 'source_type',
+			'type'    => 'select',
+			'options' => array( $this, 'get_source_type_form_field_options' ),
+		) );
+
+		$cmb->add_field( array(
+			'name'             => __( 'Organisation', 'ogdch' ),
+			'id'               => self::FIELD_PREFIX . 'organisation',
+			'type'             => 'select',
+			'options'          => array( 'Ckan_Backend_Helper', 'get_organisation_form_field_options' ),
+		) );
+
+		$cmb->add_field( array(
+			'name'    => __( 'Update frequency', 'ogdch' ),
+			'id'      => self::FIELD_PREFIX . 'update_frequency',
+			'type'    => 'select',
+			'options' => array(
+				'manual'   => __( 'Manual', 'ogdch' ),
+				'monthly'  => __( 'Monthly', 'ogdch' ),
+				'weekly'   => __( 'Weekly', 'ogdch' ),
+				'biweekly' => __( 'Biweekly', 'ogdch' ),
+				'daily'    => __( 'Daily', 'ogdch' ),
+				'always'   => __( 'Always', 'ogdch' ),
+			),
+		) );
+
+		$cmb->add_field( array(
+			'name'       => __( 'Configuration', 'ogdch' ),
+			'id'         => self::FIELD_PREFIX . 'configuration',
+			'type'       => 'textarea_code',
+			'attributes' => array( 'rows' => 3 ),
 		) );
 
 		$cmb_side_ckan = new_cmb2_box( array(
@@ -180,22 +188,36 @@ class Ckan_Backend_Local_Harvester {
 				'readonly' => 'readonly',
 			),
 		) );
+	}
 
-		/* CMB Sidebox for RDF Reference */
-		$cmb_side_ckan = new_cmb2_box( array(
-			'id'           => self::POST_TYPE . '-sidebox-rdf',
-			'title'        => __( 'RDF Reference', 'ogdch' ),
-			'object_types' => array( self::POST_TYPE ),
-			'context'      => 'side',
-			'priority'     => 'low',
-			'show_names'   => true,
-		) );
+	/**
+	 * Returns all available source types as an options array.
+	 *
+	 * @return array
+	 */
+	public static function get_source_type_form_field_options() {
+		$source_type_options = array();
 
-		/* RDF URI */
-		$cmb_side_ckan->add_field( array(
-			'name'       => __( 'RDF URI', 'ogdch' ),
-			'id'         => self::FIELD_PREFIX . 'rdf_uri',
-			'type'       => 'text',
-		) );
+		$transient_name = Ckan_Backend::$plugin_slug . '_source_types';
+		if ( false === ( $source_types = get_transient( $transient_name ) ) ) {
+			$endpoint = CKAN_API_ENDPOINT . 'harvesters_info_show';
+			$response = Ckan_Backend_Helper::do_api_request( $endpoint );
+			$errors   = Ckan_Backend_Helper::check_response_for_errors( $response );
+
+			if ( 0 === count( $errors ) ) {
+				$source_types = $response['result'];
+
+				// save result in transient
+				set_transient( $transient_name, $source_types, 1 * HOUR_IN_SECONDS );
+			} else {
+				Ckan_Backend_Helper::print_error_messages( $errors );
+			}
+		}
+
+		foreach( $source_types as $source_type ) {
+			$source_type_options[ $source_type['name'] ] = $source_type['title'];
+		}
+
+		return $source_type_options;
 	}
 }
