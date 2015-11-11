8e317cf132dd48b31274b748c25d23b234389f47
Juerg Hunziker <juerg.hunziker@liip.ch>
2015-11-11 15:18:18 +0100

    delete transient after save action

--- sync/ckan-backend-sync-local-harvester.php
+++ sync/ckan-backend-sync-local-harvester.php
@@ -44,4 +44,14 @@ class Ckan_Backend_Sync_Local_Harvester extends Ckan_Backend_Sync_Abstract {
 
 		return $data;
 	}
+
+	/**
+	 * Hook for after-sync action.
+	 *
+	 * @param object $post The post from WordPress.
+	 */
+	protected function after_sync_action( $post ) {
+		// Deletes all transients for this post-type instance.
+		delete_transient( Ckan_Backend::$plugin_slug . '_harvesters' );
+	}
 }
