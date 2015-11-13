8ae3de882715f8f14cb74efd375f5014ab5ec4e2
Juerg Hunziker <juerg.hunziker@liip.ch>
2015-11-13 14:13:56 +0100

    correct use of esc_attr_e

--- post-types/ckan-backend-local-harvester-dashboard.php
+++ post-types/ckan-backend-local-harvester-dashboard.php
@@ -111,7 +111,7 @@ class Ckan_Backend_Local_Harvester_Dashboard {
 										<label for="harvester_selection"><?php esc_html_e( __( 'Choose Harvester:', 'ogdch' ) ); ?></label>
 									</th>
 									<td>
-										<select id="harvester_selection" name="<?php esc_attr_e( $harvester_selection_field_name ); ?>">
+										<select id="harvester_selection" name="<?php echo esc_attr( $harvester_selection_field_name ); ?>">
 											<option value=""><?php esc_attr_e( '- Please choose -', 'ogdch' ); ?></option>
 											<?php
 											foreach ( $harvesters as $id => $title ) {
@@ -208,23 +208,23 @@ class Ckan_Backend_Local_Harvester_Dashboard {
 		<table class="table-small">
 			<tr>
 				<th><?php esc_attr_e( 'ID' ); ?></th>
-				<td><?php esc_attr_e( $job['id'] ); ?></td>
+				<td><?php echo esc_attr( $job['id'] ); ?></td>
 			</tr>
 			<tr>
-				<th><?php esc_attr_e( 'Created' ); ?></th>
-				<td><?php esc_attr_e( $this->convert_datetime_to_readable_format( $job['created'] ) ); ?></td>
+				<th><?php esc_attr_e( 'Created', 'ogdch' ); ?></th>
+				<td><?php echo esc_attr( $this->convert_datetime_to_readable_format( $job['created'] ) ); ?></td>
 			</tr>
 			<tr>
-				<th><?php esc_attr_e( 'Started' ); ?></th>
-				<td><?php esc_attr_e( $this->convert_datetime_to_readable_format( $job['gather_started'] ) ); ?></td>
+				<th><?php esc_attr_e( 'Started', 'ogdch' ); ?></th>
+				<td><?php echo esc_attr( $this->convert_datetime_to_readable_format( $job['gather_started'] ) ); ?></td>
 			</tr>
 			<tr>
-				<th><?php esc_attr_e( 'Finished' ); ?></th>
-				<td><?php esc_attr_e( $this->convert_datetime_to_readable_format( $job['gather_finished'] ) ); ?></td>
+				<th><?php esc_attr_e( 'Finished', 'ogdch' ); ?></th>
+				<td><?php echo esc_attr( $this->convert_datetime_to_readable_format( $job['gather_finished'] ) ); ?></td>
 			</tr>
 			<tr>
-				<th><?php esc_attr_e( 'Status' ); ?></th>
-				<td><?php esc_attr_e( $job['status'] ); ?></td>
+				<th><?php esc_attr_e( 'Status', 'ogdch' ); ?></th>
+				<td><?php echo esc_attr( $job['status'] ); ?></td>
 			</tr>
 		</table>
 		<?php
