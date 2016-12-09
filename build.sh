8be53688427edc5f81b7b642af3df2bb7ebd8164
Juerg Hunziker <juerg.hunziker@liip.ch>
2016-12-09 15:42:21 +0100

    enable travis build

--- build.sh
+++ build.sh
@@ -7,13 +7,8 @@ DIR=`dirname $SCRIPT`
 
 echo $DIR
 
-# Installation
-curl -s https://getcomposer.org/installer | php
-php composer.phar install
+# Setup WordPress codestyle
 $DIR/bin/phpcs --config-set installed_paths $DIR/vendor/wp-coding-standards/wpcs
 
 # Check code style
 $DIR/bin/phpcs -p --ignore=css,js,*/vendor/*,*/tests/* --report-width=100 --standard=$DIR/code_standard.xml $DIR
-
-# Run tests
-# $DIR/../bin/phpunit
